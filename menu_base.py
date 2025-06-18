1ad06080345cddb90107ddca375a600f92e9fb64
Valentina Vargas <val.vargasr@duocuc.cl>
2025-06-18 15:54:55 -0400

    Update menu_base.py
    Limpieza de código muerto

--- menu_base.py
+++ menu_base.py
@@ -1,14 +1,13 @@
- rama-valentina
 
 def datos_valentina():
     print("Mi nombre es Valentina Vargas, tengo 22 años")
 
 def datos_angelo():
     print("Mi nombre es Angelo ponce, tengo 29 años")
-    main
-# Menú base del programa
+
 def datos_cris():
     print("Mi nombre es Cristopher Campos, tengo 29 años")
+ # Menú base del programa
 while True:
     print("\n--- MENÚ PRINCIPAL ---")
     print("1. Función de integrante 1")
@@ -20,9 +19,7 @@ while True:
         print("Programa finalizado.")
         break
     elif op == "1":
- rama-cris
         datos_valentina()
- main
     elif op == "2":
         datos_cris()
     elif op == "3":
